6b1c9abcc8bd116d1c7d7c9ffb64e4396ebdcaf3
Eryk <erykurbas2@gmail.com>
2017-03-18 20:48:22 +0100

    Changing wyszukiwanie_liniowe() method

--- search.cpp
+++ search.cpp
@@ -11,24 +11,26 @@ void wyszukiwanie_liniowe(int tablica_do_przeszukania[] ,int poszukiwany_element
 int indeks = 0;
 int liczba_porownan = 0;
 	
-while(indeks <= (ROZMIAR_TABLICY - 1))
+while(tablica_do_przeszukania[indeks] != poszukiwany_element)
 {
 	liczba_porownan++;
-	if(tablica_do_przeszukania[indeks] == poszukiwany_element)
-	{   
-		printf("Liczba porownan: %d\n" , liczba_porownan);
-		printf("Odnaleziono element %d pod indeksem %d", poszukiwany_element, indeks);
-		break;
-	}
-	
 	indeks++;
-};
+}
+if(indeks == ROZMIAR_TABLICY)
+	{   
+		printf("Brak elementu %d w tablicy", poszukiwany_element);
+		
+	}else
+	{
+		printf("Odnaleziono element %d pod indeksem %d", poszukiwany_element, indeks);
+		printf("\nLiczba porownan: %d\n\n" , liczba_porownan);
+	}	
 }
 
 int main()
 {
 //utworzenie tablicy liczb	
-int tablica_liczb[ROZMIAR_TABLICY] = {1,4,3,2,5};
+int tablica_liczb[ROZMIAR_TABLICY] = {1,4,3,4,5};
 
 //sortowanie tablicy
 std::sort( tablica_liczb, tablica_liczb + 6 );
