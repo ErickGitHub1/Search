6ead2e80b6fdd9a90b7d16d186df7a5b415780d0
Eryk <erykurbas2@gmail.com>
2017-03-18 20:08:25 +0100

    Adding linearch search

--- search.cpp
+++ search.cpp
@@ -3,11 +3,14 @@
 #include <algorithm>
 //using namespace std;
 
+#define ROZMIAR_TABLICY 5
+
 int main()
 {
 //utworzenie tablicy liczb	
-int tablica_liczb[5] = {1,4,3,2,5};
-
+int tablica_liczb[ROZMIAR_TABLICY] = {1,4,3,2,5};
+int indeks = 0;
+int szukany_element = tablica_liczb[2];
 
 //sortowanie tablicy
 std::sort( tablica_liczb, tablica_liczb + 6 );
@@ -19,6 +22,18 @@ for(int i  = 0  ; i < 5 ; i++)
 	printf("\n");
 }		
 
+
 //wyszukiwanie liniowe
+while(indeks <= (ROZMIAR_TABLICY - 1))
+{
+	if(tablica_liczb[indeks] == szukany_element)
+	{
+		printf("Odnaleziono element %d pod indeksem %d", szukany_element, indeks);
+		break;
+	}	
+	indeks++;
+};
+
+
 return 0;
 }
