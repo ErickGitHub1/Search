377708bf017627303dd6f17ab6d24fb28f838299
Eryk <erykurbas2@gmail.com>
2017-03-19 10:50:55 +0100

    Adding wczytajTablice() method

--- search.cpp
+++ search.cpp
@@ -1,26 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <algorithm>
 
 
-//#define ROZMIAR_TABLICY 5
-int rozmiar_tablicy;
-void sortowanie_liniowe(int *, int); 
-void wyszukiwanie_liniowe(int tablica_do_przeszukania[] ,int poszukiwany_element);
 
+int rozmiar_tablicy;
+
+void sortowanie_liniowe(int *, int ); 
+void wyszukiwanie_liniowe(int *,int );
+void wczytajTablice(int* );
 
 int main()
 {
-	
+int *tablica_liczb;	
 printf("Podaj rozmiar tablicy: ");
-scanf("%d", &rozmiar_tablicy);	
+scanf("%d", &rozmiar_tablicy);
+
+ 
+tablica_liczb = (int*) malloc(rozmiar_tablicy * sizeof(*tablica_liczb));
+wczytajTablice(tablica_liczb);
+	
+
 //utworzenie tablicy liczb	
-int tablica_liczb[rozmiar_tablicy] = {1,4,3,4,5};
+//int tablica_liczb[rozmiar_tablicy] = {1,4,3,4,5};
 
 //sortowanie tablicy
 sortowanie_liniowe(tablica_liczb, rozmiar_tablicy);  
 
 //wyswietlenie posortowanej tablicy
-for(int i  = 0  ; i < 5 ; i++)
+for(int i  = 0  ; i < rozmiar_tablicy ; i++)
 {
 	printf("%d", tablica_liczb[i]);
 	printf("\n");
@@ -28,14 +36,14 @@ for(int i  = 0  ; i < 5 ; i++)
 
 wyszukiwanie_liniowe(tablica_liczb , 4);
 
-
+free(tablica_liczb);
 
 return 0;
 }
 
 
 //Funkcja dla sortowania liniowego
-void sortowanie_liniowe(int * tab, int r) 
+void sortowanie_liniowe(int *tab, int r) 
 { 
     int i, j, temp; 
     for (i = 0; i < r; i++) 
@@ -51,13 +59,14 @@ void sortowanie_liniowe(int * tab, int r)
 }
 
 //Funkcja do wyszukiwanie liniowego
-void wyszukiwanie_liniowe(int tablica_do_przeszukania[] ,int poszukiwany_element)
+void wyszukiwanie_liniowe(int *tablica_do_przeszukania ,int poszukiwany_element)
 {
 int indeks = 0;
 int liczba_porownan = 1;
 	
-while(tablica_do_przeszukania[indeks] != poszukiwany_element)
+while((tablica_do_przeszukania[indeks] != poszukiwany_element) && (indeks < rozmiar_tablicy))
 {
+	
 	liczba_porownan++;
 	indeks++;
 }
@@ -71,3 +80,13 @@ if(indeks == rozmiar_tablicy)
 		printf("\nLiczba porownan: %d\n\n" , liczba_porownan);
 	}	
 }
+
+//wczytywanie tablicy
+void wczytajTablice(int* tablica){
+     int n=rozmiar_tablicy;
+     int i=0;
+ 
+     for(i = 0 ;i<n;i++){
+                    scanf("%d",&(*tablica)+i);
+                    }     
+     }
